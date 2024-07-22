f9ccb899d1e786af18e1be23989c5bd05ce7ec64
0-light <kyk4215017@naver.com>
2024-07-22 17:51:28 +0900

    chat first way

---
+++ cactus/cactus/ChatVC.swift
@@ -0,0 +1,19 @@
+//
+//  ChatVC.swift
+//  cactus
+//
+//  Created by 김영광 on 7/22/24.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
