0c59756002bd5dbc7f35350430248f9bb9738aaf
0-light <kyk4215017@naver.com>
2024-07-22 19:54:06 +0900

    myChatUI
    just my chatUI First

--- cactus/cactus/ChatVC.swift
+++ cactus/cactus/ChatVC.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ChatVC: UIViewController {
+class ChatVC: UIViewController, UITableViewDelegate {
 
+    @IBOutlet weak var chatTalbeView: UITableView!
+    
+    var messages: [Message] = [
+        Message(sender: "Me", body: "Hello"),
+        Message(sender: "Me", body: "I love y")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        chatTalbeView.dataSource = self
+        chatTalbeView.delegate = self
+        chatTalbeView.register(UINib(nibName: "myCell", bundle: nil), forCellReuseIdentifier: "ChatCell")
+    
     }
 
 
 }
 
+extension ChatVC: UITableViewDataSource{
+     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
---
+++ cactus/cactus/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  cactus
+//
+//  Created by 김영광 on 7/22/24.
+//
+
+import UIKit
+
+struct Message{
+    let sender: String
+    let body: String
+}
